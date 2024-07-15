638a961e88be29bba5a6cd8bc2b39f2dd2f3a005
CppCXY <812125110@qq.com>
2024-07-16 00:33:57 +0800

    Fix CodeAction

--- EmmyLua/Configuration/SettingGenerateContext.cs
+++ EmmyLua/Configuration/SettingGenerateContext.cs
@@ -1,8 +1,10 @@
 ﻿using System.Text.Json.Serialization;
+using EmmyLua.CodeAnalysis.Diagnostics;
 
 namespace EmmyLua.Configuration;
 
 [JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
 [JsonSerializable(typeof(Setting))]
+[JsonSerializable(typeof(List<DiagnosticCode>))]
 public partial class SettingGenerateContext : JsonSerializerContext;
 
