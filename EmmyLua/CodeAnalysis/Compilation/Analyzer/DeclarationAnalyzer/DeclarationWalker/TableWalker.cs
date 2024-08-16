6fbfaed4d32ecf3c9d0064e4ae4badc903fce53a
CppCXY <812125110@qq.com>
2024-08-16 20:47:49 +0800

    reduce table analyzer

--- EmmyLua/CodeAnalysis/Compilation/Analyzer/DeclarationAnalyzer/DeclarationWalker/TableWalker.cs
+++ EmmyLua/CodeAnalysis/Compilation/Analyzer/DeclarationAnalyzer/DeclarationWalker/TableWalker.cs
@@ -1,17 +1,34 @@
 ﻿using EmmyLua.CodeAnalysis.Compilation.Analyzer.ResolveAnalyzer;
 using EmmyLua.CodeAnalysis.Compilation.Symbol;
+using EmmyLua.CodeAnalysis.Document;
 using EmmyLua.CodeAnalysis.Syntax.Node.SyntaxNodes;
 
 namespace EmmyLua.CodeAnalysis.Compilation.Analyzer.DeclarationAnalyzer.DeclarationWalker;
 
 public partial class DeclarationWalker
 {
+    private List<SourceRange> TableIgnoreRanges { get; } = new();
+
     private void AnalyzeTableExpr(LuaTableExprSyntax tableExprSyntax)
     {
+        if (IsIgnoreTable(tableExprSyntax))
+        {
+            return;
+        }
+
         declarationContext.TypeManager.AddDocumentElementType(tableExprSyntax.UniqueId);
         var fields = new List<LuaSymbol>();
-        foreach (var fieldSyntax in tableExprSyntax.FieldList)
+        var fieldList = tableExprSyntax.FieldList.ToList();
+        for (var i = 0; i < fieldList.Count; i++)
         {
+            var fieldSyntax = fieldList[i];
+            if (i == 0 && fieldSyntax.IsValue)
+            {
+                // declarationContext.TypeManager.AddDocumentElementType(fieldSyntax.UniqueId);
+                TableIgnoreRanges.Add(tableExprSyntax.Range);
+                return;
+            }
+
             if (fieldSyntax is { Name: { } fieldName, Value: { } value })
             {
                 var declaration = new LuaSymbol(
@@ -34,5 +51,24 @@ public partial class DeclarationWalker
         {
             declarationContext.TypeManager.AddElementMembers(tableExprSyntax.UniqueId, fields);
         }
+    }
+
+    private bool IsIgnoreTable(LuaTableExprSyntax tableExprSyntax)
+    {
+        if (TableIgnoreRanges.Count == 0)
+        {
+            return false;
+        }
+
+        var tableRange = tableExprSyntax.Range;
+        foreach (var range in TableIgnoreRanges)
+        {
+            if (range.Intersect(tableRange))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
